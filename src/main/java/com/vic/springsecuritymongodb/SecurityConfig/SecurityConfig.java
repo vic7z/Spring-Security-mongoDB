e47da4096e53c6ad024165a0fed2ebcf93b7dd6d
vic <touchvictor@gmail.com>
2021-06-01 16:26:46 +0530

    cleanup

--- src/main/java/com/vic/springsecuritymongodb/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/vic/springsecuritymongodb/SecurityConfig/SecurityConfig.java
@@ -1,22 +1,22 @@
 package com.vic.springsecuritymongodb.SecurityConfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import static com.vic.springsecuritymongodb.Roles.ApplicationRole.ADMIN;
-import static com.vic.springsecuritymongodb.Roles.ApplicationRole.STUDENT;
-
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final MyUserDetailsService userDetailsService;
+
     @Autowired
-    private MyUserDetailsService userDetailsService;
+    public SecurityConfig(MyUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
 
     @Override
@@ -26,8 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                csrf().disable()
+        http
+
                  .authorizeRequests()
 //                .antMatchers("/api/admin/**").hasRole(ADMIN.name())
 //                .antMatchers("/api/student/**").hasAnyRole(ADMIN.name(), STUDENT.name())
